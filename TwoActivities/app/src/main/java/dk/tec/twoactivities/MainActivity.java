8b772c96117f4ac0f272271504674fa93fa085ac
JanSJohansen <116347559+JanSJohansen@users.noreply.github.com>
2023-09-20 13:07:47 +0200

    Commit TwoActivities

--- TwoActivities/app/src/main/java/dk/tec/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/dk/tec/twoactivities/MainActivity.java
@@ -1,14 +1,65 @@
 package dk.tec.twoactivities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnMainGoToSecond;
+    TextView txtMainFromSecond;
+    EditText txtMainToSecond;
+    public static final String TEXT_FROM_CALLER = "TextFromCaller";
+    public static final String TEXT_FROM_SECOND = "TextFromSecond";
+    ActivityResultLauncher<Intent> secondActivityLauncher;
+    ActivityResultLauncher<Intent> thirdActivityLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMainGoToSecond = findViewById(R.id.btnMainGoToSecond);
+        txtMainFromSecond = findViewById(R.id.txtMainFromSecond);
+        txtMainToSecond = findViewById(R.id.txtMainToSecond);
+        btnMainGoToSecond.setOnClickListener(this);
+        secondActivityLauncher = registerForActivityResult
+                (
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>()
+                    {
+                        @Override
+                        public void onActivityResult(ActivityResult result)
+                        {
+                            if(result.getResultCode() == Activity.RESULT_OK)
+                            {
+                                Intent intent = result.getData();
+                                String strFromSecond =
+                                        intent.getStringExtra(MainActivity.TEXT_FROM_SECOND);
+                                txtMainFromSecond.setText(strFromSecond);
+                            }
+                        }
+                    }
+                );
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(MainActivity.TEXT_FROM_CALLER, txtMainToSecond.getText().toString());
+        //startActivity(intent);
+        secondActivityLauncher.launch(intent);
+    }
+
+
 }
--- TwoActivities/app/src/main/java/dk/tec/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/dk/tec/twoactivities/SecondActivity.java
@@ -2,13 +2,48 @@ package dk.tec.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    Intent intent;
+    Button btnSecondGoBack;
+    TextView txtSecondFromCaller;
+    EditText txtSecondToCaller;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btnSecondGoBack = findViewById(R.id.btnSecondGoBack);
+        btnSecondGoBack.setOnClickListener(this);
+
+        txtSecondFromCaller = findViewById(R.id.txtSecondFromCaller);
+        txtSecondToCaller = findViewById(R.id.txtSecondToCaller);
+
+        intent = getIntent(); // Det intent som aktiviteten er kaldt med
+        String strFromCaller = intent.getStringExtra(MainActivity.TEXT_FROM_CALLER);
+        txtSecondFromCaller.setText(strFromCaller);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        String strToCaller = txtSecondToCaller.getText().toString();
+        intent.putExtra(MainActivity.TEXT_FROM_SECOND, strToCaller);
+
+        setResult(SecondActivity.RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
     }
 }
